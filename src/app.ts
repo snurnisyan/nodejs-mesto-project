fc31509539c0f625e8b7f677fa237ec096479ee7
Tatiana Andreeva <snurnisyan@gmail.com>
2025-05-17 16:10:13 +0300

    feat: add logger

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import cardRouter from './routes/cards';
 import authMiddleware from './middlewares/auth';
 import errorHandler from './middlewares/error-handler';
 import { createUser, login } from './controllers/users';
+import { requestLogger, errorLogger } from './middlewares/logger';
 
 dotenv.config();
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
@@ -17,11 +18,16 @@ mongoose.connect(DB_URL);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(requestLogger);
 app.post('/signup', createUser);
 app.post('/signin', login);
+
 app.use(authMiddleware);
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
+
+app.use(errorLogger);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
